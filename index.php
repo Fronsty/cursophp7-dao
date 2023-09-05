683783f09a97ae7595e0eb80872f61d28a93200f
Samuel Saquete <samuel_saquete2015@outlook.com>
2023-09-05 17:23:44 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
 require_once("config.php");
+// Carrega um usuário
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root = new Usuario();
+// //Carrega uma lista de usuário
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-$root->loadById(3);
+// // Carrega uma lista de usuario buscando pelo login
+// $search = Usuario::search("us");
+// echo json_encode($search);
 
-echo $root;
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$%");
+echo $usuario;
 ?>
